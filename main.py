12c3b4d7546a4f13067bc3efc8e9ed9db9c6dd74
myrthe-vt <72264161+myrthe-vt@users.noreply.github.com>
2021-03-02 21:01:19 +0100

    updated requirements list

--- main.py
+++ main.py
@@ -2,12 +2,8 @@
 # Importing both discord.py and the commands that belong to discord.py
 import discord          #basic functions
 import numpy            #calling the random inhabitants
-import datetime         #changing seconds to hours and minutes
 import os               #accessing operating system info
 import random           #Generating random number
-import sqlite3          #Local databaseS
-import traceback        #Traceback for cogs
-import sys              #For cogs
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 
